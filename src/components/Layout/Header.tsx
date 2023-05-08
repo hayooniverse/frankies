5f2e5e7e15a8b8198184813b56a106319ab8c2e7
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2023-05-08 01:15:21 -0400

    prevent cart button from following

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -18,38 +18,14 @@ const Header = (props: any) => {
     <Fragment>
       <Provider store={store}>
         <header className={classes.header} style={{paddingRight: '200px',paddingTop :'15px'}}>
+{/* 
+           <a href="/">
+            <img src="/images/transparentfrankies.png" alt="Home" className={classes.logo} />
+          </a> */}
 
-          {/*<a href="/">*/}
-          {/*  <img src="/images/transparentfrankies.png" alt="Home" className={classes.logo} />*/}
-          {/*</a>*/}
-
-          {/*<nav>*/}
-          {/*  <ul>*/}
-          {/*    <li>*/}
-          {/*      <Link to="/menu"><button className={classes.button}>MENU</button></Link>*/}
-          {/*    </li>*/}
-          {/*  </ul>*/}
-          {/*</nav>*/}
           <HeaderCartButton onClick={props.onShowCart} style = {{paddingRight: '100px'}}/>
-          
 
-          {/*<div className={classes["profile-dropdown"]} onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>*/}
-          {/*  <a href="/">*/}
-          {/*    <img src="/images/proficon.jpg" alt="profile" className={classes.profileicon} />*/}
-          {/*  </a>*/}
-          {/*  {isDropdownOpen &&*/}
-          {/*    <div className={classes["dropdown-content"]}>*/}
-          {/*      <Link to = "/profile">*/}
-          {/*        <button className={classes.button}>Profile</button>*/}
-          {/*      </Link>*/}
-          {/*      <button className={classes.button + " " + classes["button-logout"]}>LOG OUT</button>*/}
-          {/*    </div>*/}
-          {/*  }*/}
-          {/*</div>*/}
         </header>
-        {/*<div className={classes["main-image"]}>
-          <img src={mealsImage} alt="A table full of delicious food!" />
-        </div>*/}
       </Provider>
     </Fragment>
   );
